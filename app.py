ca3d27a5965b0eedf45db60df85a85cb687c7a3a
Tareq <tareqaldhaifani@gmail.com>
2024-02-21 08:45:40 +0400

    print modules test

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import os
 import sys
+import pip
 
 
 sys.path.insert(0, os.path.dirname(__file__))
@@ -7,7 +8,10 @@ sys.path.insert(0, os.path.dirname(__file__))
 
 def app(environ, start_response):
     start_response("200 OK", [("Content-Type", "text/plain")])
-    message = help("modules")
+    installed_packages = pip.get_installed_distributions()
+    installed_packages_list = sorted(
+        ["%s==%s" % (i.key, i.version) for i in installed_packages]
+    )
     version = "Python v" + sys.version.split()[0] + "\n"
-    response = "\n".join([message, version])
+    response = "\n".join([installed_packages, version])
     return [response.encode()]
