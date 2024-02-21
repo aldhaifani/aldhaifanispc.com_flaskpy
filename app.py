8484c6b23b7a60c3049351a8111cb8c47036fb25
Tareq <tareqaldhaifani@gmail.com>
2024-02-21 08:41:11 +0400

    print modules test

--- app.py
+++ app.py
@@ -6,8 +6,8 @@ sys.path.insert(0, os.path.dirname(__file__))
 
 
 def app(environ, start_response):
-    start_response('200 OK', [('Content-Type', 'text/plain')])
-    message = os.environ.get('tg_TOKEN')
-    version = 'Python v' + sys.version.split()[0] + '\n'
-    response = '\n'.join([message, version])
+    start_response("200 OK", [("Content-Type", "text/plain")])
+    message = help("modules")
+    version = "Python v" + sys.version.split()[0] + "\n"
+    response = "\n".join([message, version])
     return [response.encode()]
