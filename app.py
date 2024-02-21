c171277ab6850476630140aa1031e7e5e7a68258
Tareq <tareqaldhaifani@gmail.com>
2024-02-21 10:59:13 +0400

    initial flask app - update

--- app.py
+++ app.py
@@ -10,6 +10,6 @@ sys.path.insert(0, os.path.dirname(__file__))
 def app(environ, start_response):
     start_response("200 OK", [("Content-Type", "text/plain")])
 
-    application = create_app()
+    application = create_app
 
-    return application
+    return [application]
